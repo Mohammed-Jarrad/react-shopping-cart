7bf1c29d17794cfbaa360e0d63b8ce22f01bca2d
Mohammed-Jarrad <abura3ed.mohammed@gmail.com>
2022-03-15 23:40:34 +0200

    header comp

---
+++ client/src/components/App/App.js
@@ -0,0 +1,18 @@
+import React from "react";
+import Header from "../Header/Header";
+
+const App = () => {
+  return (
+    <div className="layout">
+      <Header />
+      <main>
+        Main
+      </main>
+      <footer>
+        Footer
+      </footer>
+    </div>
+  )
+}
+
+export default App;
---
+++ client/src/components/Header/Header.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import '../../css/Header/Header.css'
+
+const Header = () => {
+    return (
+        <header>
+            Shopping Cart
+        </header>
+    )
+}
+
+export default Header
