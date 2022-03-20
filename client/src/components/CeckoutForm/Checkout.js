fa84de58f9d437b23a6e52c710dda96e597c4bf7
Mohammed-Jarrad <abura3ed.mohammed@gmail.com>
2022-03-20 22:54:07 +0200

    animation part 2

--- client/src/components/CeckoutForm/Checkout.js
+++ client/src/components/CeckoutForm/Checkout.js
@@ -1,33 +1,36 @@
 import React from 'react'
 import '../../css/CeckoutForm/Checkout.css'
 import Input from '../Input/Input'
+import Fade from 'react-reveal/Fade'
 
 const Checkout = ({ showForm, setShowForm, handleChange, handleSubmit }) => {
     return (
         <>
             {
                 showForm ? (
-                    <div className='checkout-form'>
-                        <span className='close-icon' onClick={() => setShowForm(false)}> &times; </span>
-                        <form onSubmit={handleSubmit}>
-                            <Input 
-                                name='name'
-                                handleChange={handleChange}
-                                type='text'
-                                label='Name'
-                            />
-                            <Input 
-                                name='email'
-                                handleChange={handleChange}
-                                type='email'
-                                label='Email'
-                            />
+                    <Fade bottom cascade>
+                        <div className='checkout-form'>
+                            <span className='close-icon' onClick={() => setShowForm(false)}> &times; </span>
+                            <form onSubmit={handleSubmit}>
+                                <Input
+                                    name='name'
+                                    handleChange={handleChange}
+                                    type='text'
+                                    label='Name'
+                                />
+                                <Input
+                                    name='email'
+                                    handleChange={handleChange}
+                                    type='email'
+                                    label='Email'
+                                />
 
-                            <div>
-                                <button> Checkout </button>
-                            </div>
-                        </form>
-                    </div>
+                                <div>
+                                    <button> Checkout </button>
+                                </div>
+                            </form>
+                        </div>
+                    </Fade>
                 ) : false
             }
         </>
